381f9cfdb772f1f1a4d76544ae034e101a1c566b
darshilshahbca <darshil.saphcm@gmail.com>
2018-04-05 02:02:54 +0530

    Manage BackButton with ViewPager

--- app/src/main/java/com/example/david/viewpager/MainActivity.java
+++ app/src/main/java/com/example/david/viewpager/MainActivity.java
@@ -48,4 +48,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        if(mPager.getCurrentItem() == 0){
+            super.onBackPressed();
+        } else{
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+
+    }
 }
